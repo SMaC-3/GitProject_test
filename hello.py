df2bde66994abee2efb6856ff2a8f59ecd3c2ccb
Josh King <josh.king@monash.edu>
2020-05-07 09:19:58 +1000

    hello commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+a = np.array([1, 2, 3])
+print(a)
+
+plt.plot([1, 2, 3], [2, 4, 6])
+plt.show()
+
+for num in [1, 2, 3, 4]:
+    print(num)
+
+
+def sqaure(x):
+
+    return x**2
